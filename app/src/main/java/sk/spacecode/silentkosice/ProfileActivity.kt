3ebbbe39561b354bd43970ebf4110cdd9e3dd6ae
Dávid Hreško <david.hresko@wirecard.com>
2019-03-30 13:57:14 +0100

    David: Activities added

---
+++ app/src/main/java/sk/spacecode/silentkosice/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package sk.spacecode.silentkosice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
